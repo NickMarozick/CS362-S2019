d0c46c1c136dca1dbff351ca5e478b51579df510
Kyle Johnson <kyledjohnson@hotmail.com>
2019-03-05 23:00:45 -0600

    Snapshot of files for Tool Library implementation
    This is just a rough draft of the files for our user story implementations. Feel free to use as a template.

---
+++ ToolLibrary/assets/js/app.js
@@ -0,0 +1,9 @@
+// Show the informational modal by clicking the navbar brand
+$('#group-brand').click(function(){
+    $('#my-modal').modal('show');
+});
+
+// Show the tool list div
+$('#view-tools-btn').click(function(){
+    $('#search-list-row').css("display", "block");
+});
